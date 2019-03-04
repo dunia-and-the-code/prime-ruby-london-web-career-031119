2d5501bf594ae779d61b6a55923e18aaa1407498
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-04 10:26:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime?(int)
   if int <= 1 
     return false
-      else
-        (2..int).to_a.all? do 
-          |counter| int % counter != 0 
+  else
+    (2..int).to_a.all? do 
+      |counter| int % counter != 0 
+  end
   end
 end
