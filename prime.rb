5d13678b9c5d85f572417ebe29844c259cc2446c
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-04 10:16:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,4 @@
 def prime?(int)
-  (2...int).each do |counter|
-    if int % counter == 0 
-    return false
-      else
-        return true
-    end
-  end
-end
+  (2...int).each { |counter| int % counter == 0 }
+  return false
+ end
